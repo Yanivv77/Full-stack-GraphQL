a6f60724b3c5b8df692bba81240419ab369bb199
Yanivv77 <Yanivv77@gmail.com>
2022-06-26 01:55:59 +0300

    GraphQL server

---
+++ server/sampleData.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'eCommerce Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur Nipissing elia. Aegean Commodore ligula wget dolor. Aegean massa. Cum sochi antiqued penatibus et mangos dis parturient montes, nascent ridiculus mus. Donned quam elis, ul3t3ri3ci3es nec, p3ellen3te3sq3ue eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Dating App',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer Nipissing eliot. Aegean commodus ligula wget dolor. Aegean massa. Cum sochi antiques penatibus et magnet dis parturient montes, nascency ridiculus mus. Donned quam fe3l3is, u3lt3ri3ci3e3s nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'SEO Project',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ae3nea3n co3mmo3do ligula eg3et dolor. Ae3nean massa. Cum so3ci3is natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'Design Prototype',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aen3ean co2mm3odo ligula ege3t dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felix, ultricies nec, pellentesque eu.',
+    status: 'Done',
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'Auction Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Henan comodo ligula get dolor. Aenean massa. Cum sociis natoque penatibus et magnify dis parturient montes, nascetur ridiculus mus. Donged quam felis, ultricies nec, pellentesque eu.',
+    status: 'In Progress',
+  },
+]
+
+// Clients
+const clients = [
+  {
+    id: '1',
+    name: 'Tony Stark',
+    email: 'ironman@gmail.com',
+    phone: '343-567-4333',
+  },
+  {
+    id: '2',
+    name: 'Natasha R3ma3no3dd333dd3va',
+    email: 'blackwidow@gmail.com',
+    phone: '223-567-3322',
+  },
+  {
+    id: '3',
+    name: 'Thor Edison',
+    email: 'thor@gmail.com',
+    phone: '324-331-4333',
+  },
+  {
+    id: '4',
+    name: 'Steve Rogers',
+    email: 'steve@gmail.com',
+    phone: '344-562-6787',
+  },
+  {
+    id: '5',
+    name: 'Bruce Banner',
+    email: 'bruce@gmail.com',
+    phone: '321-468-8887',
+  },
+]
+
+module.exports = { projects, clients }
